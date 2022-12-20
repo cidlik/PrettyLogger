f1661214e83fc6f8933fc96ec06cd2012f26462c
Roman Kuznecov <rkuznecov@elvees.com>
2022-12-20 20:48:09 +0300

    Refactoring __init__.py
    Переделал сами классы, добавил класс для Formatter.
    Теперь можно создавать различные логгеры, в т.ч., можно
    сделать отдельный логгер под запись в файл, отдельный под
    вывод в stdout.
    
    Добавил простейшие тесты.

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
 setup(
     name="PrettyLogger",
-    version="1.0.1",
+    version="1.0.2",
     description="TODO",
     packages=["PrettyLogger"],
 )
--- test_prettylogger.py
+++
@@ -1,9 +0,0 @@
-import PrettyLogger
-
-pl = PrettyLogger.PrettyLogger()
-pl.setLevel(0)
-
-pl.debug("test debug")
-pl.info("test info")
-pl.warning("test warning")
-pl.error("test error")
